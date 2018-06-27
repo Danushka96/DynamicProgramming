308bdd73fa0b5403d08113387295169121c80923
danushka96 <danushkaherath96@gmail.com>
2018-06-27 17:32:44 +0530

    fix input errors

--- src/ZigZag.java
+++ src/ZigZag.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
  * @author Danushka
  */
 public class ZigZag {
-    private static int[] subseaquence;
+    private static int[] subseaquence; //Memoization calculated results in an array
 
-    private static int setlowestHigh(int[][] array, int index){
+    private static int setlowestHigh(int[][] array, int index){ //get the lowest value which is higher than given index value
         int currentval=Integer.MIN_VALUE;
         int currentindex=-1;
         for (int i=index;i<array.length;i++){
@@ -24,7 +24,7 @@ public class ZigZag {
         return currentindex;
     }
 
-    private static int sethighesttLow(int[][] array, int index){
+    private static int sethighesttLow(int[][] array, int index){ //get the Highest value which is lower than given index value
         int currentval=Integer.MAX_VALUE;
         int currentindex=-1;
         for(int i=index;i<array.length;i++){
@@ -38,66 +38,53 @@ public class ZigZag {
                 }
             }
         }
-        //System.out.println("index:  "+currentindex+" current val: "+currentval);
         return currentindex;
     }
 
-    //return the mismatch point when a 2D array and a index is given
-    private static void checkerror(int[][] values,int index){
+    private static void checkerror(int[][] values,int index){   //return the mismatch point when a 2D array and a index is given
         int previous=values[index-1][1];
         int breakpoint=-1;
-        for (int i=index;i<values.length;i++){
+        for (int i=index;i<values.length;i++){ //Recurse from the given index
             if(previous==0){
                 if(values[i][1]==0){
                     breakpoint=i;
                     break;
                 }else{
                     previous=1;
-                    //System.out.println(values[i][0]);
                     subseaquence[i]=values[i][0];
                 }
             }else{
                 if(values[i][1]==1){
                     breakpoint=i;
-                    //System.out.println("Done");
                     break;
                 }else{
                     previous=0;
-                    //System.out.println(values[i][0]);
                     subseaquence[i]=values[i][0];
                 }
             }
         }
-        if(breakpoint!=-1){
-            //System.out.println("BP= "+breakpoint);
+        if(breakpoint!=-1){ //if Breakpoint is met
             int newindex=-1;
             if(values[breakpoint][1]==1){
                 newindex=sethighesttLow(values,breakpoint);
-                //System.out.println(values[newindex][0]+" "+values[breakpoint][0]);
-                //System.out.println("new Index= "+newindex);
-                if(newindex!=breakpoint) {
+                if(newindex!=breakpoint) { //if Breakpoint is not same as the new index
                     subseaquence[breakpoint] = values[newindex][0];
                     values[newindex][1]=0;
                 }
-
             }else{
                 newindex=setlowestHigh(values,breakpoint);
-                //System.out.println("new Index= "+newindex);
                 if(newindex!=breakpoint) {
-                    //System.out.println(values[newindex][0]+" "+values[breakpoint][1]);
                     subseaquence[breakpoint]=values[newindex][0];
                     values[newindex][1] = 1;
                 }
-
             }
             if(newindex!=values.length-1&&newindex!=breakpoint) {
-                checkerror(values, newindex);
+                checkerror(values, newindex); // Call the recursive function
             }
         }
     }
 
-    //Set Values for the 2D Array
-    private static int[][] checker(int[] values){
+    private static int[][] checker(int[] values){   //Set Values for the 2D Array
         int[][] arr=new int[values.length][2];
         for(int i=1;i<values.length;i++){
             arr[i][0]=values[i];
@@ -110,8 +97,7 @@ public class ZigZag {
         return arr;
     }
 
-    //String array to Int array
-    private static int[] setinput(String[] arr){
+    private static int[] setinput(String[] arr){  //String array to Int array
         int[] inputs=new int[arr.length];
         for (int j=0;j<arr.length;j++){
             inputs[j]=Integer.parseInt(arr[j]);
@@ -119,8 +105,7 @@ public class ZigZag {
         return inputs;
     }
 
-    //check same valued inputs and get a count of them
-    private static int getsame(int[] values){
+    private static int getsame(int[] values){  //check same valued inputs and get a count of them
         int previous=-1;
         int count=0;
         for(int num:values){
@@ -140,32 +125,25 @@ public class ZigZag {
         return count;
     }
 
-    public static void main(String args[]){
-
+    public static void main(String args[]){ //Driver Method
         Scanner inputter=new Scanner(System.in);
         // Number of test Cases
         int numberofCases=inputter.nextInt();
-
         for(int i=0;i<numberofCases;i++){
-            //number of inputs per test case
-            int inputSize=inputter.nextInt();
 
-            //input value list
-            inputter.nextLine();
+            int inputSize=inputter.nextInt(); //number of inputs per test case
+
+            inputter.nextLine(); //input value list
             String input1=inputter.nextLine();
             String[] separted=input1.split("\\s+");
 
-            //Get Inputs into Integer Array
-            int[] values=setinput(separted);
+            int[] values=setinput(separted); //Get Inputs into Integer Array
 
-            //2D array Value
-            int[][] genArray=checker(values);
-
-            //for(int[] k:genArray) System.out.println(Arrays.toString(k));
+            int[][] genArray=checker(values); //2D array Value
 
             subseaquence=new int[values.length];
             subseaquence[0]=values[0];
-            checkerror(genArray,1);
+            checkerror(genArray,1); //calling recursive Algorithm
 
             int count=0;
             for(int k:subseaquence){
@@ -174,9 +152,7 @@ public class ZigZag {
                     System.out.print(k + " ");
                 }
             }
-
             int sameinputs=getsame(subseaquence);
-            //System.out.println(sameinputs);
             System.out.println("\nLongest Sub-Sequence "+(count-sameinputs));
         }
     }
